259a4a4f4a1645a8a3fa8a7665dfd754ed4d0219
malk <daniel.malk@cloudwalk.io>
2022-11-28 17:14:52 -0300

    version 2.0.1

--- Model/Payment.php
+++ Model/Payment.php
@@ -28,7 +28,7 @@ use Magento\Framework\Math\Random;
 
 class Payment extends Cc
 {
-	const VERSION = '2.0.0';
+	const VERSION = '2.0.1';
 	const CODE = 'infinitepay';
 	protected $_code = self::CODE;
 	protected $_canAuthorize = true;
